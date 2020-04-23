55a1a289dce5ae44a19b639d9358d669feb232ed
purwaren <and.thau@gmail.com>
2020-04-24 05:52:41 +0700

    add CLock.dll

--- src/main/java/id/dailyinn/wrapper/DowsApiInterface.java
+++ src/main/java/id/dailyinn/wrapper/DowsApiInterface.java
@@ -5,6 +5,7 @@ import com.sun.jna.Native;
 
 public interface DowsApiInterface extends Library {
     DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("dcrf32", DowsApiInterface.class);
+    //DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("CLock", DowsApiInterface.class);
     int dv_connect(int beep);
     int dv_disconnect();
     int dv_check_card();
--- src/main/java/id/dailyinn/wrapper/Wrapper.java
+++ src/main/java/id/dailyinn/wrapper/Wrapper.java
@@ -6,6 +6,7 @@ public class Wrapper {
     public static void main(String args[]) {
         Logger logger = Logger.getLogger(Wrapper.class.getName());
         logger.info("Starting Dows Interface...");
+        System.setProperty("jna.library.path", "C:\\Program Files (x86)\\DOWS\\LockingSystem5.1");
         DowsApiInterface dows = DowsApiInterface.INSTANCE;
         dows.dv_connect(1);
     }
