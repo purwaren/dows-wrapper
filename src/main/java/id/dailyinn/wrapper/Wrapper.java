c168f7340121c308cd3d11a94b9bc981bdc6d54c
purwaren <and.thau@gmail.com>
2020-04-24 11:03:27 +0700

    add jnaerator.jar

--- src/main/java/id/dailyinn/wrapper/Wrapper.java
+++ src/main/java/id/dailyinn/wrapper/Wrapper.java
@@ -6,7 +6,7 @@ public class Wrapper {
     public static void main(String args[]) {
         Logger logger = Logger.getLogger(Wrapper.class.getName());
         logger.info("Starting Dows Interface...");
-        System.setProperty("jna.library.path", "C:\\Program Files (x86)\\DOWS\\LockingSystem5.1");
+        //System.setProperty("java.library.path", "C:\\Program Files (x86)\\DOWS\\LockingSystem5.1");
         DowsJniApiInterface dows = new DowsJniApiInterface();
         dows.dv_connect(1);
     }
