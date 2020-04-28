b3c56ad83474a7e838cad5a207c8d13b3acfcf0f
purwaren <and.thau@gmail.com>
2020-04-28 21:13:19 +0700

    fist working, both CLock.dll & dcrf32.dll must be present

--- src/main/java/id/dailyinn/wrapper/DowsApiInterface.java
+++ src/main/java/id/dailyinn/wrapper/DowsApiInterface.java
@@ -2,14 +2,18 @@ package id.dailyinn.wrapper;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 
 public interface DowsApiInterface extends Library {
-    DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("dcrf32", DowsApiInterface.class);
-    //DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("CLock", DowsApiInterface.class);
+    //DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("dcrf32", DowsApiInterface.class);
+    DowsApiInterface INSTANCE = (DowsApiInterface) Native.load("CLock", DowsApiInterface.class);
     int dv_connect(int beep);
     int dv_disconnect();
     int dv_check_card();
-    int dv_verify_card();
-    String dv_get_auth_code();
-    String dv_get_card_number();
+    int dv_verify_card(Pointer ctype);
+    int dv_get_auth_code(Pointer auth);
+    int dv_get_card_number(Pointer ptr);
+    int dv_read_card(Pointer auth, Pointer cardno, Pointer building, Pointer room, Pointer door, Pointer arrival, Pointer departure);
+    int dv_write_card(Pointer auth, Pointer building, Pointer room, Pointer door, Pointer arrival, Pointer departure);
+    int dv_delete_card(Pointer room);
 }
--- src/main/java/id/dailyinn/wrapper/Wrapper.java
+++ src/main/java/id/dailyinn/wrapper/Wrapper.java
@@ -2,16 +2,36 @@ package id.dailyinn.wrapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import purejavahidapi.*;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
 
-import java.util.List;
 
 public class Wrapper {
     volatile static boolean deviceOpen = false;
     public static void main(String args[]) {
         Logger logger = LoggerFactory.getLogger(Wrapper.class);
         logger.info("Starting Dows Interface...");
-        PureHidInterface hidInterface = new PureHidInterface((short) 0x0471, (short) 0xA112);
-        new Thread(hidInterface).start();
+        DowsApiInterface dows = DowsApiInterface.INSTANCE;
+        dows.dv_connect(1);
+        
+        int cardType = dows.dv_check_card();
+        logger.info("dv_check_card == {}", cardType);
+        
+        Memory mem = new Memory(Native.getNativeSize(int.class));
+        int ctype = dows.dv_verify_card(mem);
+        logger.info("[return] dv_verify_card == {}", ctype);
+        logger.info("[out] dv_verify_card == {}", mem.getInt(0));
+
+        Memory mem1 = new Memory(6);
+        dows.dv_get_auth_code(mem1);
+        logger.info("[out] dv_get_auth_code == {}", mem1.getString(0, "UTF-8"));
+
+        Memory card = new Memory(6);
+        dows.dv_get_card_number(card);
+        logger.info("[out] dv_get_card_number == {}", card.getString(0, "UTF-8"));
+
+        String authCode = "205741";
+        Memory auth = new Memory(authCode.length());
+        auth.write(0, authCode.getBytes(), 0, authCode.length());
     }
 }
